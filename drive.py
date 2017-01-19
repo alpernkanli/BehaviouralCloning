9f81298802933cea347f46c8a8b86334df001880
Alper Nebi Kanli <alpernebikanli@outlook.com>
2017-01-19 12:02:28 +0300

    Almost done.

--- drive.py
+++ drive.py
@@ -34,14 +34,15 @@ def telemetry(sid, data):
     imgString = data["image"]
     image = Image.open(BytesIO(base64.b64decode(imgString))).convert('RGB')
     image_array = np.asarray(image)
-    image_array = image_array[50:140, 0:320]
+    image_array = image_array[55:135, 0:320]
     image_array = cv2.resize(image_array, (64, 64))
+    image_array = image_array.astype(np.float32)
     image_array = image_array / 255.0 - 0.5
     transformed_image_array = image_array[None, :, :, :]
     # This model currently assumes that the features of the model are just the images. Feel free to change this.
     steering_angle = float(model.predict(transformed_image_array, batch_size=1))
     # The driving model currently just outputs a constant throttle. Feel free to edit this.
-    throttle = 0.2
+    throttle = 0.5
     print(steering_angle, throttle)
     send_control(steering_angle, throttle)
 
